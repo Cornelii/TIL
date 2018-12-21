37dd6b6e4506b583690bc7f0a0454e3b2b2bafc3
JMSON <onelife0985@gmail.com>
2018-12-21 18:10:20 +0900

    5th day

--- SSAFY/day3/urlForWorks.py
+++ SSAFY/day3/urlForWorks.py
@@ -1,6 +1,6 @@
 import webbrowser as wb
 
-telegram_api_url="https://core.telegram.org/api"
+telegram_api_url="https://core.telegram.org/bots/api"
 telegram_web_app_url="https://web.telegram.org/#/im?p=@cornelii_bot"
 
 wb.open_new_tab(telegram_api_url)
