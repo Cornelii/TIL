53b312a646fe8c69f7b4ff1156bb60f82ed2cdd8
unknown <onelife0985@gmail.com>
2019-01-13 00:50:06 +0900

    java triangle altorithm

--- Algorithms/algorithm_probs/SWEA/p6742. 삼각형 구하기/test/Solution.java
+++ Algorithms/algorithm_probs/SWEA/p6742. 삼각형 구하기/test/Solution.java
@@ -16,32 +16,38 @@ public class Solution
         
         long area;
         long count;
-        //int[]vec1=new int[2];
-        int[]vec2=new int[2];
-        long[]vec11=new long[2];
+        long[]vec12=new long[2];
 
+        int idx;
+        int idx2;
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
             N=sc.nextInt();
             A=sc.nextLong();
             B=sc.nextLong();
             int [][]points=new int[N][2]; // all the points
+            int [][]vec23=new int[(N-1)*(int)(N-2)/2][2];
             points=ref_count(N,sc);
             count=0;
 
+            for(int jmii=1;jmii<N-1;jmii++){
+                idx=(N-1)*(jmii-1)-jmii-1-(int)(jmii-1)*(jmii)/2;
+                for(int jmiii=jmii+1 ;jmiii<N;jmiii++){
+                    vec23[idx+jmiii][0]=points[jmiii][0]-points[jmii][0];
+                    vec23[idx+jmiii][1]=points[jmiii][1]-points[jmii][1];
+                }
+            }
+
             for(int jmi=0;jmi<N-2;jmi++){
                 for(int jmii=jmi+1;jmii<N-1;jmii++){
-                        // vec1=vectorize(points[jmi][0],points[jmi][1],points[jmii][0],points[jmii][1]);
-                        // vec11[0]=(long)vec1[0];
-                        // vec11[1]=(long)vec1[1];
+                        idx=(N-1)*(jmii-1)-jmii-1-(int)(jmii-1)*(jmii)/2;
 
-                        vec11[0]=points[jmii][0]-points[jmi][0];
-                        vec11[1]=points[jmii][1]-points[jmi][1];
+                        vec12[0]=points[jmii][0]-points[jmi][0];
+                        vec12[1]=points[jmii][1]-points[jmi][1];
 
                     for(int jmiii=jmii+1;jmiii<N;jmiii++){
- 
-                        vec2=vectorize(points[jmii][0],points[jmii][1],points[jmiii][0],points[jmiii][1]);
-                        area=Math.abs(vec11[0]*vec2[1]-vec11[1]*vec2[0]);
+                        idx2=idx+jmiii;
+                        area=Math.abs(vec12[0]*vec23[idx2][1]-vec12[1]*vec23[idx2][0]);
                         if(area>=A){
                             if(area<=B){
                                 count+=1;
@@ -67,12 +73,6 @@ public class Solution
     }   
     public static int[] vectorize(int p1x, int p1y, int p2x, int p2y){
         int [] vec= new int[2];
-        vec[0]=p2x-p1x;
-        vec[1]=p2y-p1y;
-        return vec;
-    }
-    public static long[] vectorize_long(int p1x, int p1y, int p2x, int p2y){
-        long [] vec= new long[2];
         vec[0]=p2x-p1x;
         vec[1]=p2y-p1y;
         return vec;
